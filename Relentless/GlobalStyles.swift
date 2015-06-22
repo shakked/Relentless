9ecfcc2525404f589f05ca93583610a05937de3d
Zachary Shakked <theshakked@gmail.com>
2015-06-22 06:26:15 -0400

    Added global styles for convenience and consistency

---
+++ Relentless/GlobalStyles.swift
@@ -0,0 +1,23 @@
+//
+//  GlobalStyles.swift
+//  Relentless
+//
+//  Created by pixable on 6/16/15.
+//  Copyright (c) 2015 Zachary Shakked. All rights reserved.
+//
+
+import UIKit
+
+class GlobalStyles: NSObject {
+    class func greenColor() -> UIColor {
+        return UIColor(rgba: "#11C99C")
+    }
+    
+    class func grayColor() -> UIColor {
+        return UIColor(rgba: "#808080")
+    }
+    
+    class func blueColor() -> UIColor {
+        return UIColor(rgba: "#0080FF")
+    }
+}

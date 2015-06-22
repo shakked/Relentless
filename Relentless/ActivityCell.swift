45dd77fa1ed5b32d3a99fcfc6b05af8bb0baeb9c
Zachary Shakked <theshakked@gmail.com>
2015-06-22 06:26:55 -0400

    activity cell

---
+++ Relentless/ActivityCell.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityCell.swift
+//  Relentless
+//
+//  Created by pixable on 6/16/15.
+//  Copyright (c) 2015 Zachary Shakked. All rights reserved.
+//
+
+import UIKit
+
+class ActivityCell: UITableViewCell {
+    
+    @IBOutlet weak var activityNameLabel: UILabel!
+    
+}

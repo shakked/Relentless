2656a4f7a53b3290f0f1d0adf66fb8ac88c9a981
Zachary Shakked <theshakked@gmail.com>
2015-06-22 06:26:26 -0400

    Changed HomeViewController to work with the journal

--- Relentless/HomeViewController.swift
+++ Relentless/HomeViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var didYouWorkOutLabel: UILabel!
     @IBOutlet weak var yesButton: UIButton!
     @IBOutlet weak var restedButton: UIButton!
+    var activityEvent : ActivityEvent
     
-    init() {
+    required init(date: NSDate) {
+        self.activityEvent = ActivityManager.sharedManager.activityEvent(date)
         super.init(nibName: "HomeViewController", bundle: NSBundle.mainBundle())
     }
     
@@ -23,27 +26,92 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.translucent = false
-        yesButton.layer.borderColor = yesButton.titleLabel?.textColor.CGColor
-        restedButton.layer.borderColor = restedButton.titleLabel?.textColor.CGColor
-        
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        configureViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func changeDate(date: NSDate) {
+        activityEvent = ActivityManager.sharedManager.activityEvent(date)
+        configureViews()
     }
-    */
+    
+    func configureViews() {
+        configureNavBar()
+        configureButtons()
+    }
+    
+    func configureNavBar() {
+        navigationController?.configureNavBar(UIColor.whiteColor(), textColor: GlobalStyles.greenColor())
+    }
+    
+    func configureButtons() {
+        yesButton.layer.borderColor = GlobalStyles.greenColor().CGColor
+        restedButton.layer.borderColor = GlobalStyles.blueColor().CGColor
+        if activityEvent.activities.count > 0 {
+            activityEvent.hasEnergyConsumer() ? selectYesButton() : selectRestedButton()
+        } else {
+            unselectYesButton()
+            unselectRestedButton()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        configureYesButtonIfNeed()
+    }
 
+    @IBAction func yesButtonPressed(sender: AnyObject) {
+        showWhatDidYouDoTable()
+    }
+    
+    @IBAction func iRestedButtonPressed(sender: AnyObject) {
+        activityEvent.has(Rest()) ? unselectRestedButton() : selectRestedButton()
+    }
+    
+    func showWhatDidYouDoTable() {
+        let wdydtvc = WhatDidYouDoTableViewController(activityEvent: activityEvent)
+        let nav = UINavigationController(rootViewController: wdydtvc)
+        nav.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+        presentViewController(nav, animated: true, completion: nil)
+    }
+    
+    func selectRestedButton() {
+        activityEvent.removeAll()
+        activityEvent.add(Rest())
+        configureYesButtonIfNeed()
+        UIView.animateWithDuration(0.50, animations: {
+            self.restedButton.backgroundColor = GlobalStyles.blueColor()
+            self.restedButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        })
+    }
+    
+    func unselectRestedButton() {
+        activityEvent.remove(Rest())
+        UIView.animateWithDuration(0.50, animations: {
+            self.restedButton.backgroundColor = UIColor.clearColor()
+            self.restedButton.setTitleColor(GlobalStyles.blueColor(), forState: .Normal)
+        })
+    }
+    
+    func configureYesButtonIfNeed() {
+        if activityEvent.hasEnergyConsumer() {
+            selectYesButton()
+            unselectRestedButton()
+        } else {
+            unselectYesButton()
+        }
+    }
+    
+    func selectYesButton() {
+        UIView.animateWithDuration(0.50, animations: {
+            self.yesButton.backgroundColor = GlobalStyles.greenColor()
+            self.yesButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        })
+    }
+    
+    func unselectYesButton() {
+        UIView.animateWithDuration(0.50, animations: {
+            self.yesButton.backgroundColor = UIColor.clearColor()
+            self.yesButton.setTitleColor(GlobalStyles.greenColor(), forState: .Normal)
+        })
+    }
 }

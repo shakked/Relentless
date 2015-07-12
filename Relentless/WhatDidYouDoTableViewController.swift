d942cdd6cb5387a0b84aeed3b90c2f8ce7f90058
Zachary Shakked <theshakked@gmail.com>
2015-07-12 13:05:57 -0400

    Refactored to use parse wrappers

--- Relentless/WhatDidYouDoTableViewController.swift
+++ Relentless/WhatDidYouDoTableViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class WhatDidYouDoTableViewController: UITableViewController {
     var activityEvent : ActivityEvent
+    var activityTypes : [ActivityType] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     let REUSE_IDENTIFIER = "cell"
+    
+    var activityTypesToCreate : [ActivityType] = []
     
     required init(activityEvent: ActivityEvent) {
         self.activityEvent = activityEvent
@@ -28,6 +35,11 @@ class WhatDidYouDoTableViewController: UITableViewController {
         navigationController?.configureNavBar(UIColor.whiteColor(), textColor: GlobalStyles.greenColor())
         title = "What did you do today?"
         configureTableView()
+        
+        ActivityManager.sharedManager.activityTypesThatConsumeEnergy { (activityTypes, succeeded) -> Void in
+            self.activityTypes = activityTypes
+        }
+        
     }
     
     func configureTableView() {
@@ -40,29 +52,37 @@ class WhatDidYouDoTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ActivityEvent.defaultActivities().count
+        return activityTypes.count
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let activityName = ActivityEvent.defaultActivities()[indexPath.row]
+        let activityType = activityTypes[indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier(REUSE_IDENTIFIER) as! ActivityCell
-        cell.activityNameLabel.text = activityName
+        cell.activityNameLabel.text = activityType.name
+        contains(activityTypesToCreate, activityType) ? configureSelectedCell(cell) : configureUnselectedCell(cell)
         
-        if activityEvent.has(activityName) {
-            configureSelectedCell(cell)
-        } else {
-            configureUnselectedCell(cell)
+        if cell.iconImageView.image == nil {
+            activityType.iconFile.getDataInBackgroundWithBlock { (data: NSData?, error: NSError?) -> Void in
+                if error == nil {
+                    cell.iconImageView.image = UIImage(data: data!)
+                }
+            }
         }
+        
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let activityName = ActivityEvent.defaultActivities()[indexPath.row]
+        let activityType = activityTypes[indexPath.row]
         
-        if activityEvent.has(activityName) {
-            activityEvent.removeActivity(activityName)
+        if contains(activityTypesToCreate, activityType) {
+            activityTypesToCreate.removeObject(activityType)
         } else {
-            activityEvent.addActivity(activityName, isEnergyConsumer: true)
+            activityTypesToCreate.append(activityType)
         }
 
         tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
@@ -81,17 +101,18 @@ class WhatDidYouDoTableViewController: UITableViewController {
     }
     
     func cancel() {
-        activityEvent.removeEnergyConsumers()
-        dismissViewControllerAnimated(true, completion: nil)
+        self.dismissViewControllerAnimated(true, completion: nil)
+
     }
     
     func done() {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-}
-
-extension UITableViewCell {
-    func clean() {
-        accessoryType = .None
+        ActivityType.createActivities(activityTypesToCreate, completion: { (succeeded, activities) -> Void in
+            if succeeded {
+                self.activityEvent.addActivities(activities)
+            } else {
+                println("failed to add activites")
+            }
+            self.dismissViewControllerAnimated(true, completion: nil)
+        })
     }
 }

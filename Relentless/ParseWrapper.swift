0c60dfb6ec17f9523de1da18e3402d5ed160f965
Zachary Shakked <theshakked@gmail.com>
2015-07-12 13:02:00 -0400

    Added a ParseWrapper protocol

---
+++ Relentless/ParseWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  ParseWrapper.swift
+//  Relentless
+//
+//  Created by pixable on 7/11/15.
+//  Copyright (c) 2015 Zachary Shakked. All rights reserved.
+//
+
+import UIKit
+
+protocol ParseWrapper {
+    init(object: PFObject)
+}

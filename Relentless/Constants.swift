fd2139d1c6c8174391cb218661c197f547ac5e98
Zachary Shakked <theshakked@gmail.com>
2015-07-12 13:01:17 -0400

    Added a Constants struct

---
+++ Relentless/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Relentless
+//
+//  Created by pixable on 7/11/15.
+//  Copyright (c) 2015 Zachary Shakked. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    struct Classes {
+        static let ActivityEvent = "ActivityEvent"
+        static let Activity = "Activity"
+        static let ActivityType = "ActivityType"
+    }
+    
+    struct Parameters {
+        static let date = "date"
+        static let user = "user"
+        static let name = "name"
+        static let activities = "activities"
+        static let isEnergyConsumer = "isEnergyConsumer"
+        static let isDefault = "isDefault"
+        static let type = "type"
+        static let icon = "icon"
+    }
+    
+    struct ActivityType {
+        static let rest = "rest"
+    }
+}

a166f909245bfbfba59c1943a8a47d69e73edc4e
Zachary Shakked <theshakked@gmail.com>
2015-07-12 13:03:47 -0400

    Added convenience methods to Array

---
+++ Relentless/Array+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Array+Extension.swift
+//  Relentless
+//
+//  Created by pixable on 7/12/15.
+//  Copyright (c) 2015 Zachary Shakked. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    mutating func removeObject<U: Equatable>(object: U) {
+        var index: Int?
+        for (idx, objectToCompare) in enumerate(self) {
+            if let to = objectToCompare as? U {
+                if object == to {
+                    index = idx
+                }
+            }
+        }
+        
+        if(index != nil) {
+            self.removeAtIndex(index!)
+        }
+    }
+}

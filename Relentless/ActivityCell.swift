7f613434db75fc758f42466b58b2bf7235ae8504
Zachary Shakked <theshakked@gmail.com>
2015-07-12 22:09:44 -0400

    Redesigned ActivityCell

--- Relentless/ActivityCell.swift
+++ Relentless/ActivityCell.swift
@@ -1,3 +1,4 @@
+
 //
 //  ActivityCell.swift
 //  Relentless
@@ -9,14 +10,36 @@
 import UIKit
 
 class ActivityCell: UITableViewCell {
+
+    var activity : Activity!
     
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var activityNameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
+    var editButtonBlock : ((Void) -> (Void))!
+    var shareButtonBlock : ((Void) -> (Void))!
+    var moreButtonBlock : ((Void) -> (Void))!
+    
+    
+    @IBAction func editButtonPressed(sender: AnyObject) {
+        editButtonBlock()
+    }
+    
+    @IBAction func shareButtonPressed(sender: AnyObject) {
+        shareButtonBlock()
+    }
+    
+    @IBAction func moreButtonPressed(sender: AnyObject) {
+        moreButtonBlock()
+    }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         iconImageView.image = nil
     }
-    
 }
 

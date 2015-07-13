65bfa33e79b80fa16d02561e7e8af5c056c217c6
Zachary Shakked <theshakked@gmail.com>
2015-07-12 22:07:18 -0400

    Added a view to display the user's streak

---
+++ Relentless/StreakView.swift
@@ -0,0 +1,16 @@
+//
+//  StreakView.swift
+//  Relentless
+//
+//  Created by pixable on 7/12/15.
+//  Copyright (c) 2015 Zachary Shakked. All rights reserved.
+//
+
+import UIKit
+
+class StreakView: UIView {
+
+    @IBOutlet weak var streakLabel: UILabel!
+    @IBOutlet weak var encouragementLabel: UILabel!
+    
+}
